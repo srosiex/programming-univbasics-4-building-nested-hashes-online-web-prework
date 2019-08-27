5503d7e037bf487af607038803cf9d12abe9c5b8
Sarah H <sarahrose.henderson@gmail.com>
2019-08-27 14:39:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
 			:four_pieces_owned => 200},
 		:names => {
 			:reading_railroad => {},
-			:set2 => "",
+			:pennsylvania_railroad => {},
 			:set3 => "",
 			:set4 => ""
 		}
