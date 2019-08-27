5eb7a6d6c68d30f1c656699f59ce3cb34177656c
Sarah H <sarahrose.henderson@gmail.com>
2019-08-27 14:26:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
 	base_hash = {:railroads => {
 		:pieces => 4,
 		:rent_in_dollars => {
-			:key1 => "",
+			:one_piece_owned => 25,
 			:key2 => "",
 			:key3 => "",
 			:key4 => ""},
